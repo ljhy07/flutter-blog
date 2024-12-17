022e13d2058cebb13389bb823fcaa271bcc45abd
이재현 <2023050@bssm.hs.kr>
2024-12-17 11:04:57 +0900

    firebase 에러 해결 / CRUD 기능 일부만 작동

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,70 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'pages/main_page.dart';
+import 'pages/write_page.dart';
+import 'pages/profile_page.dart';
+import 'components/nav.dart';
 
-void main() {
-  runApp(const ProviderScope(child: BlogApp()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    // demoProjectId: "666416714876:web:bae037f6d2e9dde66a65f9"
+  );  // Firebase 초기화
+  runApp(
+    const ProviderScope(  // 이 부분이 필수입니다
+      child: MyApp(),
+    ),
+  );
 }
 
-class BlogApp extends StatelessWidget {
-  const BlogApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Blog',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const BlogListPage(),
+      title: 'Blog App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    BlogListPage(),
+    WriteBlogPage(),
+    ProfilePage(),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Blog App")),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigation(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+      ),
     );
   }
 }
